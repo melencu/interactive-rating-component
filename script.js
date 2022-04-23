9afd81f062a1a64b12a0bfa3fb6b28dee4495434
Movicm <melencu.dev@gmail.com>
2022-04-23 14:06:35 +0300

    Added inputs instead of li elements and refactor JS code

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 "strict";
 
-//SELECT RATING
-let stars;
-const ratings = document.querySelector(".rating-list");
-const items = document.getElementsByTagName("li");
-
-for (let i = 0; i < items.length; i++)
-  items[i].addEventListener("click", function () {
-    for (let item of items) {
-      item.classList.remove("active");
-    }
-    items[i].classList.toggle("active");
-    stars = items[i].textContent;
-  });
-
 // SUBMIT RATING
 const submitCard = document.getElementById("submit-card");
 const finalCard = document.getElementById("final-card");
 const submitBtn = document.querySelector(".btn");
 submitBtn.addEventListener("click", function () {
-  submitCard.classList.toggle("hidden");
-  finalCard.classList.toggle("hidden");
-  document.querySelector(".rating").textContent = stars;
+  const rating = document.querySelector('input[name="star"]:checked').value;
+  if (rating) {
+    submitCard.classList.toggle("hidden");
+    finalCard.classList.toggle("hidden");
+    document.querySelector(".rating").textContent = rating;
+  }
 });
