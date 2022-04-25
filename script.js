75232c550b840b2c1b144e9849ee99b28566b43f
Movicm <melencu.dev@gmail.com>
2022-04-25 09:08:54 +0300

    Refactor JS code

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 "strict";
 
-// SUBMIT RATING
 const submitCard = document.getElementById("submit-card");
 const finalCard = document.getElementById("final-card");
 const submitBtn = document.querySelector(".btn");
-submitBtn.addEventListener("click", function () {
+
+// CHANGE THE BUTTON TEXT AFTER SELECTING A RATING
+const pickRating = document.querySelectorAll("label");
+for (let star of pickRating) {
+  star.addEventListener("click", () => {
+    submitBtn.textContent = "Submit";
+  });
+}
+
+// SUBMIT RATING
+submitBtn.addEventListener("click", () => {
   const rating = document.querySelector('input[name="star"]:checked').value;
   if (rating) {
     submitCard.classList.toggle("hidden");
